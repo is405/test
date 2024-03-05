bb1e00f6c84bbca6540b7ceef1c1bf550c1ecdef
ito <2000itoshg@gmail.com>
2024-03-05 22:34:24 +0900

    3/5

---
+++ JQuery/課題/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.slide').bxSlider({
+        auto:true,
+        mode:'horizontal',
+        speed:2000,
+        controls:false,
+        pager:false
+    });
+});
+
+//$(function(){
+//    $(".left-text img").hover(
+//        function(){
+//            $(this).animate({width:"200px"});
+//        },
+//        function(){
+//            $(this).animate({width:"160px"})
+//        });
+//});
---
+++ PHP/htdocs/contactform/insert.php
@@ -0,0 +1,36 @@
+<?php
+mb_internal_encoding("utf8");
+
+$pdo =new PDO("mysql:dbname=lesson01;host=localhost;", "root", "root");
+
+$pdo ->exec("insert into contactform(name,mail,age,comments)
+values('".$_POST['name']."', '".$_POST['mailaddress']."', '".$_POST['age']."', '".$_POST['comments']."');");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>お問合せフォーム</title>
+        <link rel="stylesheet" type="text/css" href="style(mail_confirm).css">
+    </head>
+    
+    <body>
+        
+        <h1>お問い合わせフォーム</h1>
+        
+        <div class="confirm">
+            <p>お問い合わせ有難うございました。
+                <br>
+                3営業日以内に担当者よりご連絡差し上げます。
+            </p>
+        </div>
+        
+        <form action="index.php">
+            <input type="submit" class="button3" value="TOPに戻る">
+        </form>
+        
+    </body>
+</html>
